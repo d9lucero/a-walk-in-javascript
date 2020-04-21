d40884f68084e1ca08603155f7648bc835bd513e
d9lucero <d9lucero@gmail.com>
2020-04-21 07:58:05 -0300

    iterables not finished

--- src/day_05/iterableAndIterators.test.js
+++ src/day_05/iterableAndIterators.test.js
@@ -4,7 +4,7 @@ describe('DAY 5: Iterable/Iterator', () => {
     it(`get the iterator function of a String`, () => {
         let string = 'hello';
         let theIteratorFunction;
-
+        theIteratorFunction=string[Symbol.iterator];
         expect(theIteratorFunction).toBeInstanceOf(Object);
         expect(theIteratorFunction).toBeInstanceOf(Function);
 
@@ -13,7 +13,7 @@ describe('DAY 5: Iterable/Iterator', () => {
     it(`get the iterator object returned by the iterator function of a String`, () => {
         let string = 'hello';
         let theIteratorObject;
-
+        theIteratorObject=string[Symbol.iterator]();
         expect(theIteratorObject).toBeInstanceOf(Object);
 
     });
@@ -21,7 +21,7 @@ describe('DAY 5: Iterable/Iterator', () => {
     it(`get the string representation
         of the iterator object returned by the iterator function of a String`, () => {
         let string = 'hello';
-        let theIteratorObjectCoercedToString;
+        let theIteratorObjectCoercedToString=string[Symbol.iterator]().toString();
 
         expect(theIteratorObjectCoercedToString).toBe('[object String Iterator]');
 
@@ -31,6 +31,7 @@ describe('DAY 5: Iterable/Iterator', () => {
         let string = 'hello';
         let theIteratorNextMethod;
 
+        theIteratorNextMethod=string[Symbol.iterator]().next;
         expect(theIteratorNextMethod).toBeInstanceOf(Object);
         expect(theIteratorNextMethod).toBeInstanceOf(Function);
 
@@ -40,12 +41,31 @@ describe('DAY 5: Iterable/Iterator', () => {
         let string = 'hello';
         let theIteratorNextMethodValue;
 
+        theIteratorNextMethodValue=string[Symbol.iterator]().next().value;
+
         expect(theIteratorNextMethodValue).toBe('h');
 
     });
 
     it(`make your own test to get all values from a String using the iterable protocol`, () => {
         // I'm throwing and error to make it fail, remove it and add your code
+        let string="Oklahoma";
+        let result=[];
+        //Let's create our own. iterator
+        string[Symbol.iterator]=undefined;
+
+        string[Symbol.iterator]= function () {
+            return{
+                next(){
+                    return{
+                        done:true,
+                        value:''
+                    }
+                }            
+            }
+        }
+
+        expect(result).toBe(['O','k','l','a','h','o','m','a']);
         throw Error('do not cheat 3:) ');
     });
 });
